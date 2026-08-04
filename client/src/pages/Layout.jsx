f44c459330207a33e0ef72f93ed35bb2f869c671
ayush <ayushojha2721@gmail.com>
2026-08-04 17:05:18 +0530

    SideBar updated

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/Sidebar";
+import SideBar from "../components/SideBar";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import Loading from "../components/Loading";
